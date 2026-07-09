7ef787f05476dc97fc2d787d131a1e4ce7edaa2d
hechkb-byte <hechkb@gmail.com>
2026-07-09 12:14:19 +0000

    Ajout du seed des villas et services

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ pip install -r requirements-prod.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Peuple la base avec les données du site (sans doublons si déjà présentes)
+python manage.py loaddata services/fixtures/services.json
+python manage.py seed_villas
